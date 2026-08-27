bfc68e957b9bc9d9aed36dfa8e49053c7173a3aa
dangtiencuong6a-cmd <dangtiencuong6a@gmail.com>
2026-08-27 13:15:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -322,7 +322,7 @@ if __name__ == '__main__':
 
     start_token_refresher()
     print("[*] Free Fire Info Site server starting on http://localhost:3000")
-    app.run(host='0.0.0.0', port=3000, debug=False)
+    app.run(host='74.220.57.0', port=3000, debug=False)
 
 
 
